93482a905c301bf921b1f935ee7d687a9a6a89a7
Deidravtr <Deidra.Driscollvtr@gmail.com>
2019-12-20 23:26:11 +0000

    Create route depending on the option the user selected (login or new user)

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+
+@app.route("/routing", methods=["GET"])
+def routing():
+    if request.values.get("login"):
+        return render_template("login.html")
+    elif request.values.get("new_user_account"):
+        return render_template("new_user_account.html")
+
+
 if __name__ == '__main__':
     app.run()
